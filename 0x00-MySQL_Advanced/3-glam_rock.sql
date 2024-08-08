a4dbd7fbbeb679dad61455b8ed7ab8c49f699e88
Vishaylinp <vishaylinparshotam@gmail.com>
2024-08-08 16:10:12 +0000

    edit

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,5 @@
 -- list all bands
 SELECT band_name, (IFNULL(split, '2020') - formed) AS lifespan
 FROM metal_bands
-WHERE style LIKE '%Glam rock%'
+WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
 ORDER BY lifespan DESC;
